1c99ac8f7758cee8232f3bacbd777ae9065bfddb
Christopher Hugh Kelly <christopher.kelly1997@gmail.com>
2023-06-24 03:47:23 -0600

    displayed the profile information is done

--- js/index.js
+++ js/index.js
@@ -1,10 +1,48 @@
+function removeApiFromUrl(url)
+{
+    if (url == undefined) return "";
+    else if (url == null) return null;
+    else if (url.length < 1) return url;
+    else
+    {
+        let indxapi = url.indexOf("api.");
+        if (indxapi < 0 || indxapi > url.length - 1) return url;
+        else
+        {
+            return url.substring(0, indxapi) + url.substring(indxapi + 4);
+        }
+    }
+}
+
+function removeUsersFromUrl(url)
+{
+    if (url == undefined) return "";
+    else if (url == null) return null;
+    else if (url.length < 1) return url;
+    else
+    {
+        let indxapi = url.indexOf("users/");
+        if (indxapi < 0 || indxapi > url.length - 1) return url;
+        else
+        {
+            return url.substring(0, indxapi) + url.substring(indxapi + 6);
+        }
+    }
+}
+
+function cleanUrl(url)
+{
+    let mytempurl = "" + removeApiFromUrl(url);
+    return removeUsersFromUrl(mytempurl);
+}
+
 document.addEventListener("DOMContentLoaded", function(){
     document.getElementById("github-form").addEventListener("submit", function(event){
         event.preventDefault();
         console.log("event.target = " + event.target);
         let usrname = "" + event.target[0].value;
         console.log("usrname = " + usrname);
-        debugger;
+        //debugger;
         
         if (usrname == undefined || usrname == null || usrname.length < 1)
         {
@@ -24,7 +62,8 @@ document.addEventListener("DOMContentLoaded", function(){
         fetch("https://api.github.com/search/users?q=" + usrname, configurationObject)
         .then(response => response.json())
         .then(function dosomething(response){
-            console.log("response = " + response);
+            //console.log("response = " + response);
+            console.log("response.items.length = " + response.items.length);
             //response is an object that contains:
             //incomplete_results: boolean true/false
             //items: array of objects
@@ -41,6 +80,82 @@ document.addEventListener("DOMContentLoaded", function(){
             //
             //total_count: # should match the array length unless the results are partial
             
+            //link to their profile
+            //their username
+            //and their avatar
+            //the onclick will open the repos_url
+            
+            //might be wise to lay it out in a table
+            //one colum for the picture and two rows for the name and link
+            //<li>
+                //<table>
+                    //<tr>
+                        //<td>image</td>
+                        //<td>
+                            //<table>
+                                //<tr><td>username</td></tr>
+                                //<tr><td>url</td></tr>
+                            //</table>
+                        //</td>
+                    //</tr>
+                //</table>
+            //</li>
+            //debugger;
+            let myusrlist = document.getElementById("user-list");
+            //for each result do the following:
+            for (let n = 0; n < response.items.length; n++)
+            {
+                console.log("response.items[" + n + "].repos_url = " +
+                    response.items[n].repos_url);
+                console.log("response.items[" + n + "].avatar_url = " +
+                    response.items[n].avatar_url);
+                console.log("response.items[" + n + "].url = " +
+                    response.items[n].url);
+                console.log("actual profile url = " + cleanUrl(response.items[n].url));
+                console.log("response.items[" + n + "].login = " +
+                    response.items[n].login);
+                
+                //let myreposa = document.createElement("a");
+                //myreposa.href = "" + response.items[n].repos_url;//repos_url value
+                //myreposa.target = "_blank";
+                let myli = document.createElement("li");
+                //myli.onclick = myreposa;
+                //console.log("myli = " + myli);
+                //console.log("myreposa = " + myreposa);
+
+                let myimgtble = document.createElement("table");
+                let myuserdatatable = document.createElement("table");
+                let myimgtblerw = document.createElement("tr");
+                let myimgtblecl = document.createElement("td");
+                let myimgtbleusrdtcl = document.createElement("td");
+                let myusrnmrw = document.createElement("tr");
+                let myurlrw = document.createElement("tr");
+                let myusrnmcl = document.createElement("td");
+                let myurlcl = document.createElement("td");
+                let myimg = document.createElement("img");
+                myimg.src = "" + response.items[n].avatar_url;//avatar_url
+                myimg.style.width = "100px";
+                myimg.style.height = "100px";
+                let myurla = document.createElement("a");
+                myusrnmcl.textContent = "" + response.items[n].login;//login
+                myurla.href = "" + cleanUrl(response.items[n].url);//url
+                myurla.target = "_blank";
+                myurla.textContent = "" + cleanUrl(response.items[n].url);//url
+                myimgtblecl.appendChild(myimg);
+                myurlcl.appendChild(myurla);
+                myurlrw.appendChild(myurlcl);
+                myusrnmrw.appendChild(myusrnmcl);
+                myuserdatatable.appendChild(myusrnmrw);
+                myuserdatatable.appendChild(myurlrw);
+                myimgtbleusrdtcl.appendChild(myuserdatatable);
+                myimgtblerw.appendChild(myimgtblecl);
+                myimgtblerw.appendChild(myimgtbleusrdtcl);
+                myimgtble.appendChild(myimgtblerw);
+                myli.appendChild(myimgtble);
+                myusrlist.appendChild(myli);
+                console.log("successfully added the data for one user!");
+                debugger;
+            }//end of n for loop
             debugger;
         })
         .catch(function (err) {
